a88ef78e5dc0516a92a52556da06064c4d848e21
xieliaan <152832209+xieliaan@users.noreply.github.com>
2023-12-04 10:20:26 +0700

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <section class="vh-100" style="background-color: #508bfc;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <div class="card-body p-5">
+
+                            <h3 class="mb-5 text-center">Halaman Index</h3>
+
+                            <h5 class="text-center">Informasi User</h5>
+                            <div class="mb-5">
+                                <p>Nama : {{ $user->name }}</p>
+                                <p>Email : {{ $user->email }}</p>
+                                <p>Password : {{ $user->password }}</p>
+                            </div>
+                            <form action="/logout" method="post">
+                                @csrf
+                                <button class="btn btn-danger btn-lg btn-block w-100" type="submit">LOGOUT</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ register.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <section class="vh-100" style="background-color: #baaa9b;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <div class="card-body p-5">
+                            <form action="/register" method="post">
+                                <h3 class="mb-5 text-center">Register Account</h3>
+    
+                                @csrf
+
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="typeEmailX-2">Nama</label>
+                                    <input type="text" name="name" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Masukkan nama" required />
+                                </div>
+    
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="typeEmailX-2">Email</label>
+                                    <input type="email" name="email" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Masukkan alamat email" required />
+                                </div>
+    
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="typePasswordX-2">Password</label>
+                                    <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Buat password baru" required />
+                                </div>
+    
+                                {{-- <!-- Checkbox -->
+                                <div class="form-check d-flex justify-content-start mb-4">
+                                    <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
+                                    <label class="form-check-label" for="form1Example3"> Remember password </label>
+                                </div> --}}
+    
+                                <button class="btn btn-primary btn-lg btn-block w-100" type="submit">Daftar Sekarang</button>
+    
+                                <p class="text-center mt-4">Sudah punya akun? <a href="/login">Masuk Disini</a></p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
